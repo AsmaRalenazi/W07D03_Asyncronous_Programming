94b836cc8176e9e869aa8e75aae48fc8b1e15ebb
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-12 08:26:20 +0300

    update app.js

--- app.js
+++ app.js
@@ -2,10 +2,3 @@ const fs = require("fs");
 const axios = require("axios");
 
 
-const app = express();
-app.use(express.json());
-
-
-app.listen(PORT, () => {
-    console.log('SERVER IS WORKING ON http://localhost:' + PORT);
-  });
