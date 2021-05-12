9c3f84f86d002f1a0a9e2f2b950f9ff9f3b4f06a
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-12 05:49:02 +0300

    q1 Practice

--- main.js
+++ main.js
@@ -71,15 +71,49 @@ const getPostAsync = async (data) => {
         console.log('ERR', err);
       });
    };
-    getPostAsync(1)
-    getPostAsync(50)
+    // getPostAsync(1)
+    // getPostAsync(50)
+
+
+//Practice
+//q1
+//Write a function appendToFile that accepts one argument data
+// and will append the passed argument as a new line to
+// the data.txt file without writing over the content in that file.
+// Check out this link from the Node JS documentation for more information.
+
+
+//use  
+//fs.writeFileSync(file, data[, options])
+const appendToFile = (data) => {
+    fs.writeFileSync(`./data.txt`,`hello`,
+    (err) => {
+        if (err) {
+          console.log(err);
+        }
+            console.log('DONE WRITE OVER THAT FILE');
+          }
+    );
+};
+  appendToFile();
+  
 
 
 
 
+  //q2
+  //Write a function copyFile that accepts one argument fileName and
+  // it would copy the file that matches the argument to the same directory 
+  //but with a modified name, data.txt would become copy_of_data.txt,
+  // it will create or overwrite copied file if exist and when it is done copying log done .
+  // Search the NodeJS file system documentation for more information.
+
+const copyFile = (fileName) => {
+    // TODO: Your code here
+  };
 
 
-    
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
